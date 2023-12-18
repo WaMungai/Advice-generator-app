c2ade52608c96c5a050d2def4719f303e3cd2358
The Passionate Programmer <wamungai10@gmail.com>
2023-12-18 18:26:28 +0300

    Add the javascript functionality

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const fetchAdvice = async () => {
   const res = await fetch("https://api.adviceslip.com/advice");
   const data = await res.json();
   document.querySelector(".title").innerHTML = `Advice #${data.slip.id}`;
-  document.querySelector("text").innerHTML = `"${data.slip.advice}"`;
+  document.querySelector(".text").innerHTML = `"${data.slip.advice}"`;
 };
 fetchAdvice();
